1e48dfcd969522dbff5bc4e306a6487ac9fa003d
Joseph Finney <TheJoeFin@users.noreply.github.com>
2024-04-11 17:55:53 -0500

    Code style and text changes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -254,11 +254,7 @@ public partial class MainWindow : Window
         ArgumentNullException.ThrowIfNull(obj);
 
         Type t = obj.GetType();
-        PropertyInfo? pi = t.GetProperty(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-        if (pi == null)
-            throw new ArgumentOutOfRangeException("propName", string.Format("Field {0} was not found in Type {1}", propName, obj.GetType().FullName));
-
+        PropertyInfo? pi = t.GetProperty(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new ArgumentOutOfRangeException(nameof(propName), string.Format("Field {0} was not found in Type {1}", propName, obj.GetType().FullName));
         return pi.GetValue(obj, null);
     }
 
