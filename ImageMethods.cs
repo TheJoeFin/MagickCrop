2c31aaaf2bd5e7bdd3e7827e5d7ecf495a49772b
Joseph Finney <TheJoeFin@users.noreply.github.com>
2024-05-03 20:02:02 -0500

    Extensions for EXIF

---
+++ ImageMethods.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Windows.Media.Imaging;
+
+namespace MagickCrop;
+public class ImageMethods
+{
+    internal static RotateFlipType GetRotateFlipType(string path)
+    {
+        using Image img = Image.FromFile(path);
+        RotateFlipType rotateFlipType = img.GetRotateFlipType();
+        return rotateFlipType;
+    }
+
+    internal static void RotateImage(BitmapImage droppedImage, RotateFlipType rotateFlipType)
+    {
+        // Only consider basic rotation for now
+        switch ((int)rotateFlipType)
+        {
+            case 1:
+                droppedImage.Rotation = Rotation.Rotate90;
+                break;
+            case 2:
+                droppedImage.Rotation = Rotation.Rotate180;
+                break;
+            case 3:
+                droppedImage.Rotation = Rotation.Rotate270;
+                break;
+            default:
+                break;
+        }
+    }
+}
